fad66b9aa2587abce38e4d323dfff0d2c2d075ac
mareerray <mareerray@gmail.com>
2025-08-20 17:29:33 +0300

    add AdventureSorcerer exercise solution

---
+++ AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,35 @@
+public class Sorcerer extends Character implements Healer {
+    private final int healCapacity;
+
+    // Constructor
+    public Sorcerer(String name, int maxHealth, int healCapacity) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    // getHealCapacity method from the Healer interface
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    // heal method from the Healer interface
+    public void heal(Character a) {
+        if (a.getCurrentHealth() == 0) {
+            return;
+        }
+        int healed = a.getCurrentHealth() + healCapacity;
+        if (healed > a.getMaxHealth()) {
+            healed = a.getMaxHealth();
+        }
+        a.setCurrentHealth(healed);
+    }
+
+    // toString method
+    @Override
+    public String toString () {
+        if (getCurrentHealth() == 0) {
+            return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", getName(), getHealCapacity());
+        }
+        return String.format("%s is a sorcerer with %d HP. It can heal %d HP.",getName(), getCurrentHealth(), getHealCapacity());
+    }
+}
